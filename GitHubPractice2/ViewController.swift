93a6fc35edbb4215f8cecf3c05bb7c881660a06f
Maya Wasilewski <mwasilewski3454@stu.d214.org>
2022-04-06 12:47:30 -0500

    code part 1

--- GitHubPractice2/ViewController.swift
+++ GitHubPractice2/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var name = ["Red Panda", "Dog", "Unicorn"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return name.count
     }
 
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = name[indexPath.row]
+        return cell
+        
+    }
 }
 
