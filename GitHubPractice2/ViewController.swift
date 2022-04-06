4d7aaade5a22eef9e714a14565b2d66b0489467e
Maya Wasilewski <mwasilewski3454@stu.d214.org>
2022-04-06 13:41:37 -0500

    fixed stuff

--- GitHubPractice2/ViewController.swift
+++ GitHubPractice2/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController, UITableViewDataSource {
 
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellView", for: indexPath)
         cell.textLabel?.text = name[indexPath.row]
         return cell
         
