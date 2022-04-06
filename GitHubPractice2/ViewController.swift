5aa4897834080a0887a95a9b522f667f38968b44
Johan Flores <jflores3123@stu.d214.org>
2022-04-06 12:29:23 -0500

    Initial Commit

---
+++ GitHubPractice2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubPractice2
+//
+//  Created by Johan Flores  on 17/01/1401 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
